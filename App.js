ea9650959f2e952f513e67d86beea103c9e58840
Mihir <mihir1053@gmail.com>
2022-09-09 10:35:31 +0530

    Commiting

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import * as React from "react";
+import React, { useState, useEffect } from 'react';
 import MapView, { Callout, Marker, Circle } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image, Button } from "react-native";
 import Slider from "@react-native-community/slider";
 import TestCallout from "./components/TestCallout";
+import * as Location from 'expo-location';
+
+
+
 export default function App() {
   const [radius, setRadius] = React.useState(1000);
   const [range, setRange] = React.useState("50%");
@@ -10,9 +14,44 @@ export default function App() {
     latitude: 19.1379608,
     longitude: 72.8397201,
   });
+  const [cPin,setCPin]=React.useState({
+    latitude: 19.2119,
+    longitude: 72.8282,
+  });
+  
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setPin(
+         {
+             latitude:location.coords.latitude,
+             longitude:location.coords.longitude
+       }
+       );
+    })();
+  }, []);
+
+ async function handlePress(){
+    let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setPin(
+         {
+             latitude:location.coords.latitude,
+             longitude:location.coords.longitude
+       }
+       );
+  }
   return (
     <View style={styles.container}>
       <MapView
+        provider='google'
         style={styles.map}
         initialRegion={{
           latitude: 19.1379608,
@@ -21,6 +60,23 @@ export default function App() {
           longitudeDelta: 0.0421,
         }}
       >
+        <Marker
+          icon={require("./assets/MarkerLogo.jpg")}
+          coordinate={cPin}
+          draggable={true}
+          onDragStart={(e) => {
+            console.log(e.nativeEvent.coordinate);
+          }}
+          onDragEnd={(e) => {
+            setCPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}
+        >
+          <Callout>
+            <TestCallout name="Mihir" price="1" rating="5"/>
+          </Callout></Marker>
         <Marker
           icon={require("./assets/MarkerLogo.jpg")}
           coordinate={pin}
@@ -35,9 +91,11 @@ export default function App() {
             });
           }}
         >
+          
           <Callout>
             <TestCallout name="Mihir" price="1" rating="5"/>
           </Callout>
+          
         </Marker>
         <Circle
           fillColor="#fcbc0533"
@@ -62,6 +120,9 @@ export default function App() {
           setRadius(100 * value);
         }}
       />
+      <View style={styles.button}>
+      <Button onPress={handlePress} title="Current Location"/>
+      </View>
     </View>
   );
 }
@@ -75,10 +136,13 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    height: 500,
   },
   callout: {
-    width: 100,
-    height: 120,
+    width: 80,
+    height: 500,
   },
+  button:{
+    marginBottom:"8%"
+  }
 });
--- components/TestCallout.js
+++ components/TestCallout.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import { View, Image,Text } from 'react-native'
+import React from "react";
+import { StyleSheet, View, Image, Text } from "react-native";
 
-export default function TestCallout({name,rating,price,image}) {
+export default function TestCallout({ name, rating, price, image }) {
   return (
-    <View style={{ width: 70, height: 120, backgroundColor: "" }}>
-			<View style={{ backgroundColor: "red", width: 70, height: 70 }}>
-				<Image source={require("../assets/CalloutImage.jpg")} style={{width:70, height:80, alignSelf:'center'}}></Image>
-			</View>
-			<Text style={{ marginTop: 5 }}>{name}</Text>
-			<Text style={{ marginTop: 3, marginLeft: 7, marginBottom:0,fontSize: 13 }}>
-				{rating} ⭐️
-			</Text>
-			<Text style={{ marginTop: -17, marginLeft: 30 }}>₹ {price}</Text>
-			
-		</View>
-  )
+    <View style={{ width: 70, height: 150, backgroundColor: "" }}>
+      <View style={{ width: 70, height: 110, borderRadius: 10 }}>
+        <Image
+          source={require("../assets/CalloutImage.jpg")}
+          style={{
+            borderRadius: 10,
+            width: 70,
+            height: 110,
+            alignSelf: "center",
+          }}
+        ></Image>
+        <Text style={{ marginTop: 8, fontWeight: "bold", fontSize: 14 }}>
+          {name}
+        </Text>
+      </View>
+      <View style={styles.rate}>
+        <Text style={{ fontSize: 13 }}>{rating} ⭐️</Text>
+        <Text>₹ {price}</Text>
+      </View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  rate: {
+	flex:1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+});
